64bf3d2986f3185d38d52481fff858b4c195e4e8
Aufa R <aufarandy18@gmail.com>
2022-09-09 10:22:18 +0700

    upload .so linux library

---
+++ Shared Object/aritmetika.h
@@ -0,0 +1,11 @@
+#ifndef __ARITMETIKA_H__
+#define __ARITMETIKA_H__
+
+extern double tambah(double a, double b);
+extern double kurang(double a, double b);
+extern double kali(double a, double b);
+extern double bagi(double a, double b);
+extern int bagi_integer(int a, int b);
+extern int sisabagi(int a, int b);
+
+#endif /* __ARITMETIKA_H__*/
